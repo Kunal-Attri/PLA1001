af7239c71085ed8ab8e42fe2187a94c54332a702
Neptune <80949976+neptune0x13@users.noreply.github.com>
2022-05-21 10:23:12 +0530

    added more than 8 bits condition

--- python/SwapTwoNibblesInAByte.py
+++ python/SwapTwoNibblesInAByte.py
@@ -2,6 +2,8 @@ from Basic_Functions import get_integer
 
 def swap(a):
     a = str(bin(a))[2:]
+    if len(a) > 8:
+        return '0b00000000'
     while len(a) < 8:
         a = '0' + a
     a = '0b' + a[4:] + a[:4]
